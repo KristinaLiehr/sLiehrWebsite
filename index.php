f1a98cdf51920d0e415cb1cfcc65146e985d0553
KristinaLiehr <46687655+KristinaLiehr@users.noreply.github.com>
2019-09-29 17:11:40 +0200

    Header outsourced

--- index.php
+++ index.php
@@ -34,65 +34,8 @@
 <html class="no-js" lang="">
 
   <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <title>Startseite</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/headstyle.css">
-    <link rel="stylesheet" type="text/css" href="css/bodystyle.css">
-    <link rel="stylesheet" type="text/css" href="css/imageHoverWithLink.css">
-    <link rel="stylesheet" type="text/css" href="css/footstyle.css">
-    <link href="https://fonts.googleapis.com/css?family=Caveat|Days+One|Homemade+Apple|Just+Another+Hand|Kalam|Maven+Pro|Molengo|Poiret+One|Prosto+One|Shadows+Into+Light"
-      rel="stylesheet">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <meta name="description" content="" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <link rel="manifest" href="site.webmanifest" />
-    <link rel="apple-touch-icon" href="icon.png" />
-    <!-- Place favicon.ico in the root directory -->
-
-    <!--<link rel="stylesheet" href="css/normalize.css" />
-    <link rel="stylesheet" href="css/main.css" />
-    <link rel="stylesheet" href="css/new.css" />-->
-
-    <header>
-        <div class="navbar">
-            <img src="Images/Logo/s. liehr LOGO.PNG" class="logo" onclick="goToHome();"/>
-            <a href="contact.html">Kontakt</a>
-            <div class="dropdown">
-                <button class="dropbtn" onclick="goToGallery();">Galerie
-                    <i class="fa fa-caret-down"></i>
-                </button>
-                <div class="dropdown-content">
-                    <a href="roofer.html">Dachdeckerarbeiten</a>
-                    <a href="carpenter.html">Zimmererarbeiten</a>
-                    <a href="facadeCladding.html">Fassadenverkleidungen</a>
-                    <a href="terraces.html">Terrassenüberdachungen</a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <button class="dropbtn" onclick="goToServices();">Leistungen
-                    <i class="fa fa-caret-down"></i>
-                </button>
-                <div class="dropdown-content">
-                    <a href="consulting.html">Beratung</a>
-                    <a href="planning.html">Planung</a>
-                    <a href="measurement.html">Aufmaß</a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <button class="dropbtn" onclick="goToAboutUs();">Über uns
-                    <i class="fa fa-caret-down"></i>
-                </button>
-                <div class="dropdown-content">
-                    <a href="company.html">Unternehmen</a>
-                    <a href="team.html">Team</a>
-                </div>
-            </div>
-            <a href="index.html">Startseite</a>
-        </div>
-    </header>
+    <?php include("header.php") ?>
   </head>
 
   <body>
